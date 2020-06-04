91440cfd10687a649e1d2e3a6268a7f2a6a3f624
mzwae <mrmonjaie@gmail.com>
2020-06-04 10:15:34 +0100

    Create customer in Stripe

--- charge.php
+++ charge.php
@@ -1,2 +1,32 @@
 <?php
-require_once('vendor/autoload.php');
+require_once('vendor\autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$STRIPE_KEY = getenv('STRIPE_SECRET');
+
+// echo $STRIPE_KEY;
+
+\Stripe\Stripe::setApiKey($STRIPE_KEY);
+
+// Sanitize Post array
+
+$POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+
+$first_name = $POST['first_name'];
+$last_name = $POST['last_name'];
+$email = $POST['email'];
+$token = $POST['stripeToken'];
+
+// echo $token;
+
+
+// Create Customer In Stripe
+$customer = \Stripe\Customer::create(array(
+    "email" => $email,
+    "source" => $token
+));
+
+
